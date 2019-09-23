96c477a050764af537cdd14b1a25dab9705e6390
AICPCU\damorej <damorej@theinstitutes.org>
2019-09-23 16:25:31 -0400

    Improve layout and interface

--- src/renderer/js/components/layout.jsx
+++ src/renderer/js/components/layout.jsx
@@ -5,11 +5,22 @@ import LeaderboardEntryList from './leaderboard/entryList';
 
 const Layout = () => {
   return <div className="layout">
-    <div className="layout__left">
-      <LeaderboardEntryForm />
+    <div className="layout__header">
+      <div className="layout__header__tab">
+        Header woo hoo
+      </div>
+      <div className="layout__header__squares"></div>
     </div>
-    <div className="layout__right">
-      <LeaderboardEntryList />
+    <div className="layout__main">
+      <div className="layout__main__left">
+        <LeaderboardEntryForm />
+      </div>
+      <div className="layout__main__right">
+        <LeaderboardEntryList />
+      </div>
+    </div>
+    <div className="layout__footer">
+      <div className="layout__footer__squares"></div>
     </div>
   </div>;
 };
