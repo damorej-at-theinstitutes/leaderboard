6343d82e5b5ec041996d767291781e520d0da685
Joe D'Amore <joe@joedamore.me>
2019-10-25 22:08:21 -0400

    Add new racers

--- src/renderer/js/racers.js
+++ src/renderer/js/racers.js
@@ -1,7 +1,15 @@
+import babyDaisy from '../images/racers/racer_icon_baby_daisy.png';
+import babyLuigi from '../images/racers/racer_icon_baby_luigi.png';
+import babyMario from '../images/racers/racer_icon_baby_mario.png';
+import babyPeach from '../images/racers/racer_icon_baby_peach.png';
+import babyRosalina from '../images/racers/racer_icon_baby_rosalina.png';
 import bowser from '../images/racers/racer_icon_bowser.png';
+import bowserJr from '../images/racers/racer_icon_bowser_jr.png';
+import catPeach from '../images/racers/racer_icon_cat_peach.png';
 import daisy from '../images/racers/racer_icon_daisy.png';
 import donkeyKong from '../images/racers/racer_icon_donkey_kong.png';
 import dryBones from '../images/racers/racer_icon_dry_bones.png';
+import dryBowser from '../images/racers/racer_icon_dry_bowser.png';
 import kingBoo from '../images/racers/racer_icon_king_boo.png';
 import koopaTroopa from '../images/racers/racer_icon_koopa_troopa.png';
 import lakitu from '../images/racers/racer_icon_lakitu.png';
@@ -10,6 +18,7 @@ import mario from '../images/racers/racer_icon_mario.png';
 import peach from '../images/racers/racer_icon_peach.png';
 import rosalina from '../images/racers/racer_icon_rosalina.png';
 import shyGuy from '../images/racers/racer_icon_shy_guy.png';
+import tanookiMario from '../images/racers/racer_icon_tanooki_mario.png';
 import toad from '../images/racers/racer_icon_toad.png';
 import toadette from '../images/racers/racer_icon_toadette.png';
 import unknown from '../images/racers/racer_icon_unknown.png';
@@ -47,8 +56,40 @@ const racers = [
     icon: rosalina,
   },
   {
+    name: 'Baby Mario',
+    icon: babyMario,
+  },
+  {
+    name: 'Baby Luigi',
+    icon: babyLuigi,
+  },
+  {
+    name: 'Baby Peach',
+    icon: babyPeach,
+  },
+  {
+    name: 'Baby Daisy',
+    icon: babyDaisy,
+  },
+  {
+    name: 'Baby Rosalina',
+    icon: babyRosalina,
+  },
+  {
+    name: 'Tanooki Mario',
+    icon: tanookiMario,
+  },
+  {
+    name: 'Cat Peach',
+    icon: catPeach,
+  },
+  {
     name: 'Bowser',
     icon: bowser,
+  },
+  {
+    name: 'Bowser Jr.',
+    icon: bowserJr,
   },
   {
     name: 'Koopa Troopa',
@@ -57,6 +98,10 @@ const racers = [
   {
     name: 'Dry Bones',
     icon: dryBones,
+  },
+  {
+    name: 'Dry Bowser',
+    icon: dryBowser,
   },
   {
     name: 'Donkey Kong',
