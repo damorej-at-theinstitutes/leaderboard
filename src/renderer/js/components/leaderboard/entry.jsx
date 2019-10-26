f1fa3b5545e3ddb6c7894b1cdd7c1f5167ad2b8d
Joe D'Amore <joe@joedamore.me>
2019-10-25 22:42:59 -0400

    Add entry rank images and styling

--- src/renderer/js/components/leaderboard/entry.jsx
+++ src/renderer/js/components/leaderboard/entry.jsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 
 import Racers from '../../racers';
+import Ranks from '../../ranks';
 
 const LeaderboardEntry = (props) => {
 
-  console.log(props);
+  const rankClass = `entry__rank entry__rank--${props.rank}`;
+
+  function getRankImage(rank) {
+    let imagePath = Ranks[rank - 1].icon;
+
+    return <img src={imagePath} alt={Ranks[rank - 1].name} />;
+  }
 
   return <div className="entry">
     <div className="entry__content">
-      <div className="entry__rank">
-        { props.rank }
+      <div className={rankClass}>
+        { getRankImage(props.rank) }
       </div>
       <div className="entry__icon">
         <img src={Racers[props.racer].icon} />
---
+++ src/renderer/js/ranks.js
@@ -0,0 +1,55 @@
+import first from '../images/ui/ranks/rank-1.png';
+import second from '../images/ui/ranks/rank-2.png';
+import third from '../images/ui/ranks/rank-3.png';
+import fourth from '../images/ui/ranks/rank-4.png';
+import fifth from '../images/ui/ranks/rank-5.png';
+import sixth from '../images/ui/ranks/rank-6.png';
+import seventh from '../images/ui/ranks/rank-7.png';
+import eighth from '../images/ui/ranks/rank-8.png';
+import ninth from '../images/ui/ranks/rank-9.png';
+import tenth from '../images/ui/ranks/rank-10.png';
+
+const ranks = [
+  {
+    name: '1st',
+    icon: first,
+  },
+  {
+    name: '2nd',
+    icon: second,
+  },
+  {
+    name: '3rd',
+    icon: third,
+  },
+  {
+    name: '4th',
+    icon: fourth,
+  },
+  {
+    name: '5th',
+    icon: fifth,
+  },
+  {
+    name: '6th',
+    icon: sixth,
+  },
+  {
+    name: '7th',
+    icon: seventh,
+  },
+  {
+    name: '8th',
+    icon: eighth,
+  },
+  {
+    name: '9th',
+    icon: ninth,
+  },
+  {
+    name: '10th',
+    icon: tenth,
+  },
+];
+
+export default ranks;
