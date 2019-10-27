e279da7003a9c8813aa1755097201cdc6b8fb8b5
Joe D'Amore <joe@joedamore.me>
2019-10-27 13:41:41 -0400

    Improve time formatter output

--- src/renderer/js/timeFormatter.js
+++ src/renderer/js/timeFormatter.js
@@ -6,28 +6,33 @@
  * Time: Time (in milliseconds) to format.
  * Digits: Max number of digits to display.
  */
-const timeFormatter = (time, digits) => {
+const timeFormatter = (time, digits, padSeconds = true) => {
   const maxSeconds = parseInt('9'.repeat(digits), 10);
-
   const seconds = Math.floor(time / 1000);
   const points = Math.floor((time % 1000) / 10);
 
-  // Short-circuit if seconds exceeds maxSeconds.
+  // Short-circuit if time is maxed.
   if (seconds > maxSeconds) {
-    return `${maxSeconds}:99`;
+    return {
+      seconds: `${maxSeconds}`,
+      points: '99',
+    };
   }
 
   let secondsPad = '';
   let pointsPad = '';
 
-  if (seconds.toString().length < digits) {
+  if (padSeconds && seconds.toString().length < digits) {
     secondsPad = '0'.repeat(digits - seconds.toString().length);
   }
   if (points.toString().length < 2) {
     pointsPad = '0';
   }
 
-  return `${secondsPad}${seconds}:${pointsPad}${points}`;
+  return {
+    seconds: `${secondsPad}${seconds}`,
+    points: `${pointsPad}${points}`,
+  };
 }
 
 export default timeFormatter;
